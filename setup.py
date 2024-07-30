be50200e275d840ae76bf2889170bc8fe152701b
Annie Gray <73651756+anniegray52@users.noreply.github.com>
2024-07-30 14:27:56 +0100

    pypi

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup
 
 setup(name='pyemb',
-      version='1.0.0-alpha',
-          author='Annie Gray',
-    author_email='annie.gray@bristol.ac.uk',
+      version='1.0.0a0',
+      author='Annie Gray',
+      author_email='annie.gray@bristol.ac.uk',
       description='EDA for complex data',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-#     url='https://github.com/yourusername/your_project',
-    # packages=find_packages(),
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.6',
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
+      url='https://github.com/pyemb/pyemb',
+      # packages=find_packages(),
+      classifiers=[
+          'Programming Language :: Python :: 3',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+      ],
+      python_requires='>=3.6',
       install_requires=['numpy', 'pandas']
       )
    
