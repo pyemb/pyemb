5f53b541751cfd6ecd4f43915a431937fdd47f5d
edwarddavis1 <edwarddavis@hotmail.co.uk>
2024-11-25 16:56:24 +0000

    made numba an optional dependency

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -11,4 +11,13 @@ def test_form_omni_matrix(dtype):
 
     omni_matrix = eb._utils._form_omni_matrix(As, n, T)
 
+    # Check the shape of the omni_matrix
     assert omni_matrix.shape == (n * T, n * T)
+
+    # Check that the diagonal blocks are equal to the original matrices
+    for t in range(T):
+        start_idx = t * n
+        end_idx = (t + 1) * n
+        np.testing.assert_array_equal(
+            omni_matrix[start_idx:end_idx, start_idx:end_idx], As[t]
+        )
